712d2e1f58322f7508fb54401eeb5fea4f050732
jsebmej13 <juan.mejia026@misena.edu.co>
2022-11-08 17:01:23 -0500

    Project 08/11

---
+++ proyectoFormativo/js/signup.js
@@ -0,0 +1,9 @@
+$('#weekYear').datepicker({
+    todayHighlight: true,
+    format: "mm-yyyy",
+    viewMode: "month",
+    minViewMode: "month"
+  });
+  $("#weekYear").on("changeDate", function(event) {
+  
+  });
