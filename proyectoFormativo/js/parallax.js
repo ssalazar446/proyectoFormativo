cb616e882d6bf82c90b055f35c1a26551428f5dc
ssalazar446 <ssalazar446@misena.edu.co>
2022-12-22 19:58:29 -0500

    new footer and bugs fixing

---
+++ proyectoFormativo/js/parallax.js
@@ -0,0 +1,42 @@
+gsap.from("#m1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+gsap.from("#m2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+gsap.from("#t2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: -50,
+})
+gsap.from("#t1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+gsap.from("#man", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: -150,
+})
+gsap.from("#plants", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: -50,
+})
+gsap.from("#text", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 400,
+})
